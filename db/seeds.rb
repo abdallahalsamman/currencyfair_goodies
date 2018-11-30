399e2da0256d0fc867a8682dbd3cc5e6a9e0de57
Abdallah Samman <sammanabdallah@gmail.com>
2018-12-01 00:35:50 +0200

    Add fake data generator to populate db; improve frontend font appearance;

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Adding transactions to db"
+puts "This might take up to a minute..."
+10_000.times do
+  Transaction.create(:user_id => Faker::Number.number(6), :currency_from => Faker::Currency.code, :currency_to => Faker::Currency.code,
+                     :amount_sell => Faker::Number.between(1, 10_000), :amount_buy => Faker::Number.between(1, 10_000),
+                     :rate => Faker::Number.decimal(0, 4).to_f * 10, :time_placed => Faker::Date.between(10.years.ago, Date.today).to_s,
+                     :originating_country => Faker::Address.country_code)
+end
+puts "10,000 transactions added to db :)\n"
