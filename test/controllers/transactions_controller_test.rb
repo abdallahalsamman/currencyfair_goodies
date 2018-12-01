bdec6e092be49f31040f2c3e021bd247dbf71cc8
Abdallah Samman <sammanabdallah@gmail.com>
2018-12-01 09:47:33 +0200

    fix api path in tests; test yearly_report;

--- test/controllers/transactions_controller_test.rb
+++ test/controllers/transactions_controller_test.rb
@@ -6,41 +6,46 @@ class TransactionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get yearly_report" do
+    get transactions_yearly_report_path
+    assert_response :success
+  end
+
   test "should accept transaction" do
-    post '/api/process_transaction', params: {userId: "134256", currencyFrom: "EUR", currencyTo: "GBP", "amountSell": 1000,
+    post api_process_transaction_path, params: {userId: "134256", currencyFrom: "EUR", currencyTo: "GBP", "amountSell": 1000,
                    amountBuy: 747.10, rate: 0.7471, timePlaced: "24-JAN-15 10:27:44", originatingCountry: "FR"}
     assert_response :success
   end
 
   test "should reject transaction with same currencyFrom and currencyTo" do
     # same currencyFrom and currencyTo
-    post '/api/process_transaction', params: {userId: "134256", currencyFrom: "GBP", currencyTo: "GBP", "amountSell": 1000,
+    post api_process_transaction_path, params: {userId: "134256", currencyFrom: "GBP", currencyTo: "GBP", "amountSell": 1000,
                    amountBuy: 747.10, rate: 0.7471, timePlaced: "24-JAN-15 10:27:44", originatingCountry: "FR"}
     assert_response :bad_request
   end
 
   test "should reject transaction with amountSell or amountBuy as a string" do
-    post '/api/process_transaction', params: {userId: "134256", currencyFrom: "GBP", currencyTo: "EUR", "amountSell": 'wrong_type',
+    post api_process_transaction_path, params: {userId: "134256", currencyFrom: "GBP", currencyTo: "EUR", "amountSell": 'wrong_type',
                    amountBuy: 747.10, rate: 0.7471, timePlaced: "24-JAN-15 10:27:44", originatingCountry: "FR"}
     assert_response :bad_request
 
-    post '/api/process_transaction', params: {userId: "134256", currencyFrom: "GBP", currencyTo: "EUR", "amountSell": 123.1,
+    post api_process_transaction_path, params: {userId: "134256", currencyFrom: "GBP", currencyTo: "EUR", "amountSell": 123.1,
                    amountBuy: 'wrong_type', rate: 0.7471, timePlaced: "24-JAN-15 10:27:44", originatingCountry: "FR"}
     assert_response :bad_request
   end
 
   test "should reject transaction with currencyFrom or currencyTo as invalid currency name" do
-    post '/api/process_transaction', params: {userId: "134256", currencyFrom: "GBPA", currencyTo: "EUR", "amountSell": 1000,
+    post api_process_transaction_path, params: {userId: "134256", currencyFrom: "GBPA", currencyTo: "EUR", "amountSell": 1000,
                    amountBuy: 747.10, rate: 0.7471, timePlaced: "24-JAN-15 10:27:44", originatingCountry: "FR"}
     assert_response :bad_request
 
-    post '/api/process_transaction', params: {userId: "134256", currencyFrom: "EUR", currencyTo: "GBPP", "amountSell": 1000,
+    post api_process_transaction_path, params: {userId: "134256", currencyFrom: "EUR", currencyTo: "GBPP", "amountSell": 1000,
                    amountBuy: 747.10, rate: 0.7471, timePlaced: "24-JAN-15 10:27:44", originatingCountry: "FR"}
     assert_response :bad_request
   end
 
   test "should reject transaction with originatingCountry as invalid short country code" do
-    post '/api/process_transaction', params: {userId: "134256", currencyFrom: "EUR", currencyTo: "GBP", "amountSell": 1000,
+    post api_process_transaction_path, params: {userId: "134256", currencyFrom: "EUR", currencyTo: "GBP", "amountSell": 1000,
                    amountBuy: 747.10, rate: 0.7471, timePlaced: "24-JAN-15 10:27:44", originatingCountry: "FRCA"}
     assert_response :bad_request
   end
